b10f6dd6ac5246031e9adab56a882e65f4ff33aa
joealdersonstrachan <joealdersonstrachan@strachan.email>
2025-04-17 14:24:14 +0100

    feat: initial commit

---
+++ src/schemas/index.ts
@@ -0,0 +1,127 @@
+import organisationValidationSchemaCreateOrganisationJson from './organisationValidationSchemaCreateOrganisation.json';
+
+import organisationValidationSchemaUpdateOrganisationJson from './organisationValidationSchemaUpdateOrganisation.json';
+
+import organisationValidationSchemaReadOrganisationJson from './organisationValidationSchemaReadOrganisation.json';
+
+import organisationValidationSchemaDeleteOrganisationJson from './organisationValidationSchemaDeleteOrganisation.json';
+
+import organisationRoleValidationSchemaCreateOrganisationRoleJson from './organisationRoleValidationSchemaCreateOrganisationRole.json';
+
+import organisationRoleValidationSchemaUpdateOrganisationRoleJson from './organisationRoleValidationSchemaUpdateOrganisationRole.json';
+
+import organisationRoleValidationSchemaReadOrganisationRoleJson from './organisationRoleValidationSchemaReadOrganisationRole.json';
+
+import organisationRoleValidationSchemaDeleteOrganisationRoleJson from './organisationRoleValidationSchemaDeleteOrganisationRole.json';
+
+import organisationTypeValidationSchemaCreateOrganisationTypeJson from './organisationTypeValidationSchemaCreateOrganisationType.json';
+
+import organisationTypeValidationSchemaUpdateOrganisationTypeJson from './organisationTypeValidationSchemaUpdateOrganisationType.json';
+
+import organisationTypeValidationSchemaReadOrganisationTypeJson from './organisationTypeValidationSchemaReadOrganisationType.json';
+
+import organisationTypeValidationSchemaDeleteOrganisationTypeJson from './organisationTypeValidationSchemaDeleteOrganisationType.json';
+
+import organisationUserValidationSchemaCreateOrganisationUserJson from './organisationUserValidationSchemaCreateOrganisationUser.json';
+
+import organisationUserValidationSchemaUpdateOrganisationUserJson from './organisationUserValidationSchemaUpdateOrganisationUser.json';
+
+import organisationUserValidationSchemaReadOrganisationUserJson from './organisationUserValidationSchemaReadOrganisationUser.json';
+
+import organisationUserValidationSchemaDeleteOrganisationUserJson from './organisationUserValidationSchemaDeleteOrganisationUser.json';
+
+import roleValidationSchemaCreateRoleJson from './roleValidationSchemaCreateRole.json';
+
+import roleValidationSchemaUpdateRoleJson from './roleValidationSchemaUpdateRole.json';
+
+import roleValidationSchemaReadRoleJson from './roleValidationSchemaReadRole.json';
+
+import roleValidationSchemaDeleteRoleJson from './roleValidationSchemaDeleteRole.json';
+
+import userValidationSchemaCreateUserJson from './userValidationSchemaCreateUser.json';
+
+import userValidationSchemaUpdateUserJson from './userValidationSchemaUpdateUser.json';
+
+import userValidationSchemaReadUserJson from './userValidationSchemaReadUser.json';
+
+import userValidationSchemaDeleteUserJson from './userValidationSchemaDeleteUser.json';
+
+import userSessionValidationSchemaCreateUserSessionJson from './userSessionValidationSchemaCreateUserSession.json';
+
+import userSessionValidationSchemaUpdateUserSessionJson from './userSessionValidationSchemaUpdateUserSession.json';
+
+import userSessionValidationSchemaReadUserSessionJson from './userSessionValidationSchemaReadUserSession.json';
+
+import userSessionValidationSchemaDeleteUserSessionJson from './userSessionValidationSchemaDeleteUserSession.json';
+
+import userTypeValidationSchemaCreateUserTypeJson from './userTypeValidationSchemaCreateUserType.json';
+
+import userTypeValidationSchemaUpdateUserTypeJson from './userTypeValidationSchemaUpdateUserType.json';
+
+import userTypeValidationSchemaReadUserTypeJson from './userTypeValidationSchemaReadUserType.json';
+
+import userTypeValidationSchemaDeleteUserTypeJson from './userTypeValidationSchemaDeleteUserType.json';
+
+export const organisationValidationSchemaCreateOrganisation = organisationValidationSchemaCreateOrganisationJson;
+
+export const organisationValidationSchemaUpdateOrganisation = organisationValidationSchemaUpdateOrganisationJson;
+
+export const organisationValidationSchemaReadOrganisation = organisationValidationSchemaReadOrganisationJson;
+
+export const organisationValidationSchemaDeleteOrganisation = organisationValidationSchemaDeleteOrganisationJson;
+
+export const organisationRoleValidationSchemaCreateOrganisationRole = organisationRoleValidationSchemaCreateOrganisationRoleJson;
+
+export const organisationRoleValidationSchemaUpdateOrganisationRole = organisationRoleValidationSchemaUpdateOrganisationRoleJson;
+
+export const organisationRoleValidationSchemaReadOrganisationRole = organisationRoleValidationSchemaReadOrganisationRoleJson;
+
+export const organisationRoleValidationSchemaDeleteOrganisationRole = organisationRoleValidationSchemaDeleteOrganisationRoleJson;
+
+export const organisationTypeValidationSchemaCreateOrganisationType = organisationTypeValidationSchemaCreateOrganisationTypeJson;
+
+export const organisationTypeValidationSchemaUpdateOrganisationType = organisationTypeValidationSchemaUpdateOrganisationTypeJson;
+
+export const organisationTypeValidationSchemaReadOrganisationType = organisationTypeValidationSchemaReadOrganisationTypeJson;
+
+export const organisationTypeValidationSchemaDeleteOrganisationType = organisationTypeValidationSchemaDeleteOrganisationTypeJson;
+
+export const organisationUserValidationSchemaCreateOrganisationUser = organisationUserValidationSchemaCreateOrganisationUserJson;
+
+export const organisationUserValidationSchemaUpdateOrganisationUser = organisationUserValidationSchemaUpdateOrganisationUserJson;
+
+export const organisationUserValidationSchemaReadOrganisationUser = organisationUserValidationSchemaReadOrganisationUserJson;
+
+export const organisationUserValidationSchemaDeleteOrganisationUser = organisationUserValidationSchemaDeleteOrganisationUserJson;
+
+export const roleValidationSchemaCreateRole = roleValidationSchemaCreateRoleJson;
+
+export const roleValidationSchemaUpdateRole = roleValidationSchemaUpdateRoleJson;
+
+export const roleValidationSchemaReadRole = roleValidationSchemaReadRoleJson;
+
+export const roleValidationSchemaDeleteRole = roleValidationSchemaDeleteRoleJson;
+
+export const userValidationSchemaCreateUser = userValidationSchemaCreateUserJson;
+
+export const userValidationSchemaUpdateUser = userValidationSchemaUpdateUserJson;
+
+export const userValidationSchemaReadUser = userValidationSchemaReadUserJson;
+
+export const userValidationSchemaDeleteUser = userValidationSchemaDeleteUserJson;
+
+export const userSessionValidationSchemaCreateUserSession = userSessionValidationSchemaCreateUserSessionJson;
+
+export const userSessionValidationSchemaUpdateUserSession = userSessionValidationSchemaUpdateUserSessionJson;
+
+export const userSessionValidationSchemaReadUserSession = userSessionValidationSchemaReadUserSessionJson;
+
+export const userSessionValidationSchemaDeleteUserSession = userSessionValidationSchemaDeleteUserSessionJson;
+
+export const userTypeValidationSchemaCreateUserType = userTypeValidationSchemaCreateUserTypeJson;
+
+export const userTypeValidationSchemaUpdateUserType = userTypeValidationSchemaUpdateUserTypeJson;
+
+export const userTypeValidationSchemaReadUserType = userTypeValidationSchemaReadUserTypeJson;
+
+export const userTypeValidationSchemaDeleteUserType = userTypeValidationSchemaDeleteUserTypeJson;
